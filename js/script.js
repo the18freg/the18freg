d3563a7022a4e8b9eb30850672f7cb59c118b10d
the18freg <YOUR_GITHUB_EMAIL>
2026-07-06 20:03:13 +0530

    Updated product images

--- js/script.js
+++ js/script.js
@@ -1,25 +1,39 @@
-emailjs.init({
-  publicKey: "4lyndCBtAZ2AV1gvh",
-});
+document.addEventListener("DOMContentLoaded", function () {
 
-const form = document.getElementById("contact-form");
-
-if (form) {
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-
-    emailjs.sendForm(
-      "service_dev7b5f",
-      "template_g268yfu",
-      this
-    )
-    .then(() => {
-      alert("✅ Thank you! Your message has been sent successfully.");
-      form.reset();
-    })
-    .catch((error) => {
-      console.error(error);
-      alert("❌ Failed to send message.");
+    emailjs.init({
+        publicKey: "4lyndCBtAZ2AV1gvh"
     });
-  });
-}
+
+    const form = document.getElementById("contact-form");
+
+    if (form) {
+
+        form.addEventListener("submit", function (e) {
+
+            e.preventDefault();
+
+            emailjs.sendForm(
+                "service_dev7b5f",
+                "template_g268yfu",
+                this
+            )
+            .then(function () {
+
+                alert("✅ Message Sent Successfully!");
+
+                form.reset();
+
+            })
+            .catch(function (error) {
+
+                console.error("EmailJS Error:", error);
+
+                alert("❌ Message send failed.");
+
+            });
+
+        });
+
+    }
+
+});
